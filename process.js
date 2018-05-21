5bcb103508573df212eb9c6d947f9e5df7935909
Stephen Lavelle <analytic@gmail.com>
2018-05-21 04:05:00 +0200

    rss feed

--- process.js
+++ process.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs')
 var path = require('path')
+var RSS = require('rss')
 
 // STEP 1 : generate folders
 
@@ -9,10 +10,10 @@ var exec = require('child_process').execSync
 var execAsync = require('child_process').exec
 exec("rm -rf output")
 exec("mkdir output")
-exec("mkdir output/thumbs")
 exec("mkdir output/game")
 exec("mkdir output/icos")
-exec("cp templates/privacy.html output/privacy.html")
+execAsync("cp templates/privacy.html output/privacy.html")
+execAsync("cp -r symbols output/symbols")
 
 // STEP 2 : read in CSV
 
@@ -42,6 +43,8 @@ var table = rows.map( r => r.split(";").map( s => s.trim().replace(/±/gi,';')).
 
 table.shift();
 
+var pageNames=[];
+
 // STEP 3 : generate sub files
 for (var i=0;i<table.length;i++){
 	var r = table[i]
@@ -63,25 +66,32 @@ for (var i=0;i<table.length;i++){
 	var src_desc = r[10]
 	var flash = r[11]
 	var zip = r[12]
-	var unityplayer = r[13]
+	var unity = r[13]
 
 	var datesplit = date.split('-')
 	var datenum = parseInt(datesplit[0])*10000+parseInt(datesplit[1])*100+parseInt(datesplit[2]);
-	r.push(datenum);
+	r.push(datenum); [14]
 
 	var safeName = title.replace(/[ ]/g,'-').replace(/[^a-zA-Z0-9-_\.]/g,'')
-	var pageName = date+"-"+safeName+".html";
+	var pageName = safeName+".html";
+
+	if (pageNames.indexOf(pageName)>=0){
+		var c = 2;	
+		while (pageNames.indexOf(pageName)>=0){
+			pageName = safeName+"_"+c+".html";
+			c++;
+		}
+	}
+	pageNames.push(pageName);
 
 
 	if (icon==""){
 		icon = date+"-"+safeName+".png";
-		var convertcmd = `convert output/icos/${icon}  -strip -alpha Remove -interpolate Nearest -filter point -resize 100x100\! output/thumbs/${icon}`
-		execAsync(`./generateicon.js ${safeName} output/icos/${icon}; ${convertcmd}; pngcrush -ow output/thumbs/${icon}`)
+		execAsync(`./generateicon.js ${safeName} output/icos/${icon}`)
 		r[2] = icon
 	} else {
 		// -interpolate Nearest -filter point
-		var convertcmd = `convert output/icos/${icon} -strip -alpha Remove -resize 100x100\! output/thumbs/${icon}`
-		execAsync(`cp icos/${icon} output/icos/${icon} ; pngcrush -ow output/icos/${icon}; ${convertcmd}`)				
+		execAsync(`cp icos/${icon} output/icos/${icon}`)				
 	}
 
 	bodyMarkdown = htmlToMarkdown(desc)
@@ -108,8 +118,8 @@ for (var i=0;i<table.length;i++){
 		if (flash!=""){
 			result += `${pre} <a href="${flash}">Play Now</a> ${post} (Flash) \n`
 		}
-		if (unityplayer!=""){
-			result += `${pre} <a href="${flash}">Play Now</a> ${post} (Unity Web Player) \n`
+		if (unity!=""){
+			result += `${pre} <a href="${unity}">Play Now</a> ${post} (Unity Web Player) \n`
 		}
 		if (src!=""){
 			result += `${presrc} <a href="${src}">Download Source Code</a> ${post} (${src_desc}) \n`
@@ -118,8 +128,8 @@ for (var i=0;i<table.length;i++){
 	}
 
 
-	r.push(pageName)
-
+	r.push(pageName)//[15]
+	r.push(niceDate)//[16]
 	var page=eval(postTemplate);
 	fs.writeFile("output/game/"+pageName,page);
 
@@ -148,22 +158,189 @@ function doGrid(){
 		var title = r[0]
 		var date = r[1]
 		var icon = r[2]
-
-
+		var caption = r[3]
+		var desc = r[4]
+		var html = r[5]
+		var mac = r[6]
+		var win = r[7]
+		var linux = r[8]
+		var src = r[9]
+		var src_desc = r[10]
+		var flash = r[11]
+		var zip = r[12]
+		var unity = r[13]
+		var datenum = r[14]
 		var pageName = r[15]
+		var niceDate = r[16]
 
-		if (i%6===0){
-			s+="\t<tr>\n"
-		}
+		var cardTemplate = `<div class="card">
+		<a href="game/${pageName}">
+        <img class="thumb" width="250px" height="250px" src="icos/${icon}">
+        <div class="date">${niceDate}</div >
+        <div class="gamename">${title}</div>
+        </a>
+        `;
 
-		s+=`\t\t<td><a href="game/${pageName}"><img class="ico" width="100" height="100" src="thumbs/${icon}"><br>${title}</a>`
+        if (html!=""){
+        	cardTemplate += `
+		          <div class="container">
+		            <a href="${html}" title="Play Now (HTML5)" alt="Play Now (HTML5)" >
+		              <img width="50px" height="50px" class="icon" src="symbols/html5.svg" width="50px" height="50px">        
+		              <div class="overlay">
+		                <div class="text">HTML5</div>
+		              </div>
+		            </a>
+		          </div>`
+      	}
 
-		s+="\n"
+        if (win!=""){
+        	cardTemplate += `
+		          <div class="container">
+		            <a href="${win}" title="Download for Windows" alt="Download for Windows" >
+		              <img width="50px" height="50px" class="icon" src="symbols/windows.svg" width="50px" height="50px">        
+		              <div class="overlay">
+		                <div class="text">WIN</div>
+		              </div>
+		            </a>
+		          </div>`
+      	}
 
 
+        if (mac!=""){
+        	cardTemplate += `
+		          <div class="container">
+		            <a href="${mac}" title="Download for macOS" alt="Download for macOS" >
+		              <img width="50px" height="50px" class="icon" src="symbols/apple.svg" width="50px" height="50px">        
+		              <div class="overlay">
+		                <div class="text">MAC</div>
+		              </div>
+		            </a>
+		          </div>`
+      	}
+
+
+        if (linux!=""){
+        	cardTemplate += `
+		          <div class="container">
+		            <a href="${linux}" title="Download for Linux" alt="Download for Linux" >
+		              <img width="50px" height="50px" class="icon" src="symbols/linux.svg" width="50px" height="50px">        
+		              <div class="overlay">
+		                <div class="text">LINUX</div>
+		              </div>
+		            </a>
+		          </div>`
+      	}
+
+        if (flash!=""){
+        	cardTemplate += `
+		          <div class="container">
+		            <a href="${flash}"  title="Play Online Now (Flash)" alt="Play Online Now (Flash)">
+		              <img width="50px" height="50px" class="icon" src="symbols/flash.svg" width="50px" height="50px">        
+		              <div class="overlay">
+		                <div class="text">FLASH</div>
+		              </div>
+		            </a>
+		          </div>`
+      	}
+
+        if (zip!=""){
+        	cardTemplate += `
+		          <div class="container">
+		            <a href="${zip}">
+		              <img width="50px" height="50px" class="icon" title="Download Zip File" alt="Download Zip File" src="symbols/zip.svg" width="50px" height="50px">        
+		              <div class="overlay">
+		                <div class="text">ZIP</div>
+		              </div>
+		            </a>
+		          </div>`
+      	}
+
+        if (unity!=""){
+        	cardTemplate += `
+		          <div class="container">
+		            <a href="${unity}">
+		              <img width="50px" height="50px" class="icon" title="Play Online Now (Unity Web Player)" alt="Play Online Now (Unity Web Player)" src="symbols/unity.svg" width="50px" height="50px">        
+		              <div class="overlay">
+		                <div class="text">UNITY</div>
+		              </div>
+		            </a>
+		          </div>`
+      	}
+
+        if (src!=""){
+        	cardTemplate += `
+		          <div class="container">
+		            <a href="${src}">
+		              <img width="50px" height="50px" class="icon" title="Download Source Code (${src_desc})" alt="Download Source Code (${src_desc})" src="symbols/source.svg" width="50px" height="50px">        
+		              <div class="overlay">
+		                <div class="text">SOURCE</div>
+		              </div>
+		            </a>
+		          </div>`
+      	}
+
+      	s+="</div>"
+		s+=cardTemplate;		
 	}
 	return s;
 }
 
 var page=eval(indexTemplate)
 fs.writeFile("output/index.html",page)
+
+
+var feedOptions = {
+	title:"increpare games",
+	description:"a feed of games and other things made by increpare",
+	feed_url:"https://www.increpare.com/feed.rss",
+	site_url:"https://www.increpare.com",
+	image_url:"https://www.increpare.com/favicon.png",
+	author:"Stephen Lavelle",
+	categories: ['games','increpare','indie games','open source'],
+	webMaster:"Stephen Lavelle",
+	copyright:"2018 Stephen Lavelle",
+	managingEditor:"Stephen Lavelle",
+	language: 'en'
+};
+
+var feed = new RSS(feedOptions);
+
+for (var i=0;i<Math.min(20,table.length);i++){
+	var r = table[i];
+
+	var title = r[0]
+	var date = r[1]
+	var icon = r[2]
+	var caption = r[3]
+	var desc = r[4]
+	var html = r[5]
+	var mac = r[6]
+	var win = r[7]
+	var linux = r[8]
+	var src = r[9]
+	var src_desc = r[10]
+	var flash = r[11]
+	var zip = r[12]
+	var unity = r[13]
+	var datenum = r[14]
+	var pageName = r[15]
+	var niceDate = r[16]
+
+	var splitDate = niceDate.split('/');
+	var year = splitDate[0];
+	var month = splitDate[1];
+	var day = splitDate[2];
+	var date = new Date(year,month,date)
+
+	var itemOptions = {
+		title:title,
+		description:caption,
+		url:`https://www.increpare.com/games/${pageName}`,
+		date:date.toUTCString()		
+	}
+	feed.item(itemOptions);
+
+	var xml = feed.xml();
+
+	fs.writeFile("output/feed.rss",xml);
+}
